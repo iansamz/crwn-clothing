e9acb25368fa405527c0b7f8aaf664c3970e2759
Ian Mungai <iansam74@gmail.com>
2021-08-13 19:51:44 +0300

    images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,67 @@
 import React from 'react';
-import './directory.styles.scss'
+import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends React.Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-            ]              
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl: '/menu/hats.jpg',
+          id: 1,
+          linkUrl: 'shop/hats',
+        },
+        {
+          title: 'jackets',
+          imageUrl: '/menu/jackets.jpg',
+          id: 2,
+          linkUrl: 'shop/jackets',
+        },
+        {
+          title: 'sneakers',
+          imageUrl: '/menu/sneakers.jpg',
+          id: 3,
+          linkUrl: 'shop/sneakers',
+        },
+        {
+          title: 'womens',
+          imageUrl: '/menu/women.jpg',
+          size: 'large',
+          id: 4,
+          linkUrl: 'shop/womens',
+        },
+        {
+          title: 'mens',
+          imageUrl: '/menu/men.jpg',
+          size: 'large',
+          id: 5,
+          linkUrl: 'shop/mens',
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { sections } = this.state;
+    return (
+      <div className="directory-menu">
+        {
+          sections.map((section) => (
+            <MenuItem
+              key={section.id}
+              title={section.title}
+              imageUrl={section.imageUrl}
+              linkUrl={section.linkUrl}
+              size={section.size}
+            />
+          ))
         }
-    }
-
-    render() {
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({title, imageUrl, id, linkUrl, size}) => (
-                        <MenuItem  key={id} title={title} imageUrl={imageUrl} linkUrl={linkUrl} size={size} />
-                    ))
-                }
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default Directory
+export default Directory;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const MenuItem = ({ title, imageUrl }) =>{
-    
-    return (
-        <div className="menu-item" style={{
-            background: `url(${imageUrl})`
-        }}>
-            <div className="content">
-                <h1 className="title">{ title }</h1>
-                <span className="subtitle">BUY NOW</span>
-            </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    className={`${size} menu-item`}
+    style={{
+      background: `url(${imageUrl})`,
+    }}
+  >
+    <div className="content">
+      <h1 className="title">{ title }</h1>
+      <span className="subtitle">BUY NOW</span>
     </div>
-    )
-}
+  </div>
+);
 
-export default MenuItem
+export default MenuItem;
