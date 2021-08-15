b1ea726a04c66f5615797a5baeac1901a4f44235
Ian Mungai <iansam74@gmail.com>
2021-08-15 11:06:29 +0300

    shop data import

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,46 +1,14 @@
 import React from 'react';
 import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component';
+import { SECTION_DATA } from '../../core/data/section.data';
 
 class Directory extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: '/menu/hats.jpg',
-          id: 1,
-          linkUrl: 'shop/hats',
-        },
-        {
-          title: 'jackets',
-          imageUrl: '/menu/jackets.jpg',
-          id: 2,
-          linkUrl: 'shop/jackets',
-        },
-        {
-          title: 'sneakers',
-          imageUrl: '/menu/sneakers.jpg',
-          id: 3,
-          linkUrl: 'shop/sneakers',
-        },
-        {
-          title: 'womens',
-          imageUrl: '/menu/women.jpg',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens',
-        },
-        {
-          title: 'mens',
-          imageUrl: '/menu/men.jpg',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens',
-        },
-      ],
+      sections: SECTION_DATA
     };
   }
 
---
+++ src/core/data/section.data.js
@@ -0,0 +1,34 @@
+export const SECTION_DATA= [
+    {
+      title: 'hats',
+      imageUrl: '/menu/hats.jpg',
+      id: 1,
+      linkUrl: 'shop/hats',
+    },
+    {
+      title: 'jackets',
+      imageUrl: '/menu/jackets.jpg',
+      id: 2,
+      linkUrl: 'shop/jackets',
+    },
+    {
+      title: 'sneakers',
+      imageUrl: '/menu/sneakers.jpg',
+      id: 3,
+      linkUrl: 'shop/sneakers',
+    },
+    {
+      title: 'womens',
+      imageUrl: '/menu/women.jpg',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens',
+    },
+    {
+      title: 'mens',
+      imageUrl: '/menu/men.jpg',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens',
+    },
+  ];
