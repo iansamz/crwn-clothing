fe38e290da9e59f7b0fccf7b196a17bb6e13d976
Ian Mungai <iansam74@gmail.com>
2021-08-15 12:05:29 +0300

    collection item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/home.component'; 
 import ShopPage from './pages/shop/shop.component'; 
@@ -7,8 +7,10 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/shop" component={ShopPage}/>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/shop" component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
